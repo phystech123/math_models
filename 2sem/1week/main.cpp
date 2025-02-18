a2e9f999410cd469736dd805db98eb922ff95217
Oleg <riabov.oe@phystech.edu>
2025-02-18 10:33:12 +0300

    temp

--- 2sem/1week/main.cpp
+++ 2sem/1week/main.cpp
@@ -2,6 +2,45 @@
 #include<cmath>
 #include<random>
 
+
+// //first task
+// int main(){
+//     int k = 0;
+//     std::cin >> k;
+//     std::random_device r;
+//     std::default_random_engine e1(r());
+//     std::uniform_int_distribution<int> uniform_dist(1, k);
+//     std::cout << uniform_dist(e1) << std::endl;
+//     std::cout << uniform_dist(e1) << std::endl;
+//     std::cout << uniform_dist(e1) << std::endl;
+//     return 0;
+// }
+
+// //second task
+// int main(){
+//     int k = 0;
+//     std::cin >> k;
+//     std::random_device r;
+//     std::default_random_engine e1(r());
+//     std::uniform_real_distribution<> uniform_dist(1, k);
+//     std::cout << uniform_dist(e1) << std::endl;
+//     std::cout << uniform_dist(e1) << std::endl;
+//     std::cout << uniform_dist(e1) << std::endl;
+//     return 0;
+// }
+
+//third task
+// int main(){
+//     std::random_device r;
+//     std::default_random_engine e1(r());
+//     std::normal_distribution<> normal_dist(100, 10);
+//     for(int i = 0; i < 10; i++){
+//         std::cout << normal_dist(e1) << std::endl;
+//     }
+//     return 0;
+// }
+
+//fourth task
 int main(){
     
     return 0;
